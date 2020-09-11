6ad7da578e41a9ec65b554b379d08b4e7d9ae71f
Rattapong Deejaingam <PNdemon@hotmail.com>
2020-09-11 19:12:36 +0700

    การบ้านครั้งที่ 2

---
+++ app/src/main/java/com/su/cp/homework1/Show.java
@@ -0,0 +1,45 @@
+package com.su.cp.homework1;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Show extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+        Button checkButton = findViewById(R.id.check);
+
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText idEditText = findViewById(R.id.id_edit_text);
+                String strID = idEditText.getText().toString();
+                if(strID.length() != 13){
+                    Toast t = Toast.makeText(Show.this, "กรุณากรอกหมายเลขบัตรประชาชน 13 หลัก", Toast.LENGTH_LONG);
+                    t.show();
+                }
+                else if(strID.equals("1111111111111") || strID.equals("2222222222222")){
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(Show.this);
+                    dialog.setTitle("ผลการตรวจสอบสิทธิ์");
+                    dialog.setMessage("คุณเป็นผู้มีสิทธิ์เลือกตั้ง");
+                    dialog.setPositiveButton("OK" , null);
+                    dialog.show();
+                }else{
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(Show.this);
+                    dialog.setTitle("ผลการตรวจสอบสิทธิ์");
+                    dialog.setMessage("คุณไม่มีสิทธิ์เลือกตั้ง");
+                    dialog.setPositiveButton("OK" , null);
+                    dialog.show();
+                }
+            }
+        });
+    }
+}
